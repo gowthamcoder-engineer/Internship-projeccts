d56fdade201dbf2e6a2992e66ddc380852be342b
Gowthamgowda m <mg908261@gmail.com>
2026-03-17 20:39:59 +0530

    final project implementation

---
+++ test.py
@@ -0,0 +1,11 @@
+import whisper
+import torch
+import customtkinter
+import sounddevice
+import numpy
+import openpyxl
+import ollama
+import transformers
+from PIL import Image
+
+print("All libraries working")
